662aad873cb85adebac2655e5d8556854d1ec423
wkjung0624 <>
2021-07-06 09:44:15 +0900

    Add Vue.js Practice
    레스토랑 페이지 예제

--- restaurant/src/views/Restaurant.vue
+++ restaurant/src/views/Restaurant.vue
@@ -1,65 +1,98 @@
 <template>
-    <div>
-        <div>
+    <div class="container">
+        <div class="MENU_BOARD">
         <!-- 상품 아이템 리스트 -->
-            <button v-for="menuItem in menus"
-            @click="sendOrder(menuItem.menuId);">
-                {{menuItem.menuName}}<br>
-                ({{menuItem.price}})
+            <button v-for="(menuItem, menuIdx) in menus" v-bind:key=menuIdx
+                @click="sendOrder(menuItem.menuId)"
+                class="btn">
+                <span style="color: rgb(60, 255, 0);">{{menuItem.menuName}}</span><br>
+                <span style="color: white;">({{menuItem.price.toLocaleString()}} 원)</span>
             </button>
         </div>
-    </div>
-
-    <div>
+    
         <!-- 주문 목록 -->
-        <table>
-            <thead>
-                <tr>
-                    <th> 메뉴2명 </th>
-                    <th> 가격 </th>
-                    <th> 수량 </th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- qty값이 음수일 때 방지 필요 -->
-                <tr :key=orderIdx v-for="(orderItem, orderIdx) in currentOrderList">
-                    <td style="width:150px; font-size:12px"> {{orderItem.menuName}} </td>
-                    <td style="width:100px; font-size:12px"> {{orderItem.price}} </td>
-                    <td> 
-                        <input type="number" v-model="orderItem.qty" style="width:30px;">
-                    </td>
-                    <td> <button @click="deleteOrder(orderIdx)"> X </button> </td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- 할인 전 총 금액 -->
         <div>
-            <h1>{{totalAmount}}</h1>
+            <table class="MN_ORDER_BOOK">
+                <thead>
+                    <tr>
+                        <th class="th th-head01"> 메뉴명 </th>
+                        <th class="th th-head02"> 가격 </th>
+                        <th class="th th-head03"> 수량 </th>
+                        <th class="th th-head02"> 총합 </th>
+                        <th class="th th-head03"> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- qty값이 음수일 때 방지 필요 -->
+                    <tr :key=orderIdx v-for="(orderItem, orderIdx) in currentOrderList">
+                        <td class="item-font"> {{orderItem.menuName}} </td>
+                        <td class="item-font"> {{orderItem.price.toLocaleString()}} 원</td>
+                        <td><input type="number" style="width:30px;"
+                            v-model.number="orderItem.qty" 
+                            @change="changeOrder(orderIdx)">
+                        </td>
+                        <td class="item-font"> {{orderItem.total.toLocaleString()}} 원 </td>
+                        <td> <button @click="deleteOrder(orderIdx)"> X </button> </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- 할인 전 총 금액 -->
+        <div class="result">
+            <p class="big-white">할인 전 금액 : {{totalAmount.toLocaleString()}} 원</p>
         </div>
 
         <!-- 할인 쿠폰, 카드 선택 -->
-        <div>
-            쿠폰 선택 및 카드 선택
-            <select v-for="(cardType, cardIdx) in cardTypes">
-                <option value="1" v-for="creditItem in creditCards">
-                    {{creditItem.cardName}}</option>
-            </select>
-        </div>
+        <div class="MENU_BOARD">
+            <p class="big-white">쿠폰 선택 및 카드 선택 </p>
+            <div>
+                <select v-model="selectedCouponId"
+                        style="width:200px;"
+                        @change="payment()">
+                    <option v-bind:value=0>
+                        {{ paddingBothSide("할인쿠폰",21,"=") }}
+                    </option>
+                    <!-- v-bind:value 에 함수를 집어넣어 호출마다 1, 2, 3 ... n 을 반환할 때 왜 coupons의 크기만큼 호출하지 않는가? 
+                        너무 많은 횟수를 호출함 -->
+                    <option v-bind:key=cpIdx+1
+                            v-bind:value=cpIdx+1
+                            v-for="(cpItem, cpIdx) in coupons">
+                            
+                            {{cpItem.title}}
 
+                    </option>
+                </select>
+            </div>
+        
+
+
+
+        <div v-bind:key=ctIdx v-for="(ctItem, ctIdx) in cardTypes">
+            <select v-model="selectedCardIdList[ctIdx]" 
+                    @change="payment()" 
+                    style="width:200px;">
+                <option v-bind:value=0> {{paddingBothSide(ctItem.title,21,"=")}} </option>
+                <option v-bind:key=fctIdx+1
+                        v-bind:value=fctIdx+1
+                        v-for="(fctItem, fctIdx) in getFilteredCards(ctItem.cardType)">
+                            {{fctItem.cardName}}
+                </option>
+            </select>
+
+            <!-- creditCards -> discount, discountType -->
+        </div>
+        </div>
         <!-- 최종 금액 -->
-        <div>
-            <h3> 최종 결제액 </h3>
+        <div class="result">
+            <p class="big-white"> 최종 결제액 : {{discounted.toLocaleString()}} 원</p>
         </div>
     </div>
-
 </template>
 
 <script>
 export default {
     data(){
         return {
-            // totalAmmount : 0,
             //상품 정보
             menus : [{
                 menuId: 1,
@@ -278,33 +311,129 @@ export default {
                 discountType: "%"
             }
             ],
+            //할인 쿠폰
+            coupons : [{
+                couponId: 1,
+                title: "5% 할인쿠폰(중복할인 가능)",
+                discount: 5,
+                doubleDiscount: true,
+                discountType: "%"
+                },
+                {
+                couponId: 2,
+                title: "10% 할인쿠폰(중복할인 가능)",
+                discount: 10,
+                doubleDiscount: true,
+                discountType: "%"
+                },
+                {
+                couponId: 3,
+                title: "15% 할인쿠폰(중복할인 가능)",
+                discount: 15,
+                doubleDiscount: true,
+                discountType: "%"
+                },
+                {
+                couponId: 4,
+                title: "5000 할인쿠폰(중복할인 가능)",
+                discount: 5000,
+                doubleDiscount: true,
+                discountType: ""
+                },
+                {
+                couponId: 5,
+                title: "10,000 할인쿠폰(중복할인 가능)",
+                discount: 10000,
+                doubleDiscount: true,
+                discountType: ""
+                },
+                {
+                couponId: 6,
+                title: "20,000 할인쿠폰(중복할인 가능)",
+                discount: 20000,
+                doubleDiscount: true,
+                discountType: ""
+                },
+                {
+                couponId: 7,
+                title: "5% 할인쿠폰(중복할인 불가능)",
+                discount: 5,
+                doubleDiscount: false,
+                discountType: "%"
+                },
+                {
+                couponId: 8,
+                title: "10% 할인쿠폰(중복할인 불가능)",
+                discount: 10,
+                doubleDiscount: false,
+                discountType: "%"
+                },
+                {
+                couponId: 9,
+                title: "15% 할인쿠폰(중복할인 불가능)",
+                discount: 15,
+                doubleDiscount: false,
+                discountType: "%"
+                },
+                {
+                couponId: 10,
+                title: "5000 할인쿠폰(중복할인 불가능)",
+                discount: 5000,
+                doubleDiscount: false,
+                discountType: ""
+                },
+                {
+                couponId: 11,
+                title: "10,000 할인쿠폰(중복할인 불가능)",
+                discount: 10000,
+                doubleDiscount: false,
+                discountType: ""
+                },
+                {
+                couponId: 12,
+                title: "20,000 할인쿠폰(중복할인 불가능)",
+                discount: 20000,
+                doubleDiscount: false,
+                discountType: ""
+                }
+            ],
 
 
-            //현재 주문한 상품 정보 
+            // 현재 주문한 상품 정보 
             // menuid 를 인덱스로 값들을 가지는걸로
             currentOrderList : {},
+
+            selectedCardIdList : Array.from({length: 4}, () => 0),
+            selectedCouponId : 0,
+
+            totalAmount : 0,
+            discounted : 0,
         };
     },
-    mounted(){
+    watch: {
+        currentOrderList : {
+            deep:true,
+            
+            handler(){
+                let result = 0;
+                const tempList = this.currentOrderList
 
-    },
-    computed: {
-        // getTotalAmount() {
-        //     console.log("amnt");
-        //     const reducer = (accumulator, curValue) => accumulator + curValue.price * curValue.qty
-        //     this.totalAmount = this.currentOrderList.reduce(reducer)
-        // }
-        totalAmount(){
-            if (Object.keys(this.currentOrderList).length > 0){
-                //console.table("###",this.currentOrderList, "####")
-                const reducer = (accumulator, curValue) => accumulator + curValue.price * curValue.qty
-                for (var i in this.currentOrderList){
-                    console.log(this.currentOrderList[i])
+                if (Object.keys(tempList).length > 0){
+                    
+                    for (var idx in tempList){
+                        result += tempList[idx].total
+                    }
+                    this.totalAmount = result
                 }
-                return 1 //this.currentOrderList.reduce(reducer)
+                else {
+                    this.totalAmount = 0
+                }
             }
-            else {
-                return 0
+        },
+
+        totalAmount : {
+            handler(){
+                this.payment()
             }
         }
     },
@@ -318,27 +447,204 @@ export default {
             
             if(isKeyExist){
                 this.currentOrderList[`${menuId}`].qty += 1;
+                this.currentOrderList[`${menuId}`].total = price * this.currentOrderList[`${menuId}`].qty;
             }
+
             else{
                 this.currentOrderList[menuId] = {
                     menuName : name,
                     price : price,
                     qty : 1,
+                    total : price,
                 };
             }
-            console.table(this.currentOrderList);
         },
         
-        deleteOrder(delItem){
-            delete this.currentOrderList[delItem];
+        deleteOrder(orderId){
+            delete this.currentOrderList[orderId];
         },
 
+        changeOrder(orderId){
+            // 딥카피 안되있는 변수
+            var oldItem = this.currentOrderList[orderId]
+            this.payment()
 
+            if (oldItem.qty <= 0){
+                this.deleteOrder(orderId)
+            }else{
+                this.currentOrderList[orderId].total = oldItem.price * oldItem.qty
+            }
+            
+        },
+        
+        // 할인율, 할인 타입, 할인 전 금액을 받아 최종 할인된 금액을 반환함
+        // getDiscountValue
+        getDiscountPrice(discount, discountType, oldPrice){
+            
+            let result = 0;
+
+            switch(discountType){
+                case '%':
+                    result = oldPrice * (1 - (discount * 0.01))
+                    return (result) > 0 ? result : 0;
+                case '':
+                    result = oldPrice - discount;
+                    return (result) > 0 ? result : 0;
+                default:
+                    console.log("discountType is not '%' or ''")
+                    return -1
+            }
+        },
+
+        payment(){
+            
+            var oldAmount = this.totalAmount;
+            var Discount_Results = [];
+                                    
+            // 선 쿠폰 후 카드 계산
+            // 쿠폰 정보 부터 가져온다.
+
+            if(this.selectedCouponId > 0){
+                var couponItem = this.coupons.filter(item => item.couponId == this.selectedCouponId)[0]
+                var total_coupon_discount = this.getDiscountPrice(couponItem.discount, couponItem.discountType, oldAmount)
+            
+                // 쿠폰이 중복할인이 가능한지 확인한다.
+                // 가능하면 전체금액 감소 이후 카드 계산
+                if(couponItem.doubleDiscount){
+                    oldAmount = total_coupon_discount;
+                    console.log("double discounted", oldAmount, total_coupon_discount)
+                }else{
+                    // 중복할인 불가하면 카드별 최대 할인 비교해야하므로 배열에 푸쉬
+                    // 쿠폰명, 할인금액(할인전금액 - 할인액) 푸쉬
+                    Discount_Results.push(oldAmount - total_coupon_discount)
+                }
+            }
+
+
+            // 카드 정보를 가져온다.
+            // 이전 카드 할인율이 현재 카드 할인율보다 높으면 가장 좋은 카드 Value 갱신
+
+            
+            var g_idx = 0;
+            var idx = 0;
+
+            for(var credit of this.cardTypes){
+                var cards = this.creditCards.filter(i => i.cardType == credit.cardType)
+                var cardId = this.selectedCardIdList[idx]
+
+                idx += 1;
+
+                if(cardId != 0){
+                    var items = cards.filter(i => i.cardId == (g_idx + cardId))[0]
+                    var total_credit_discount = this.getDiscountPrice(items.discount, items.discountType, oldAmount)
+
+                    console.table(items)
+                    Discount_Results.push(oldAmount - total_credit_discount)     
+                }
+
+                g_idx += cards.length;
+            }
+
+            
+            
+            // 쿠폰 중복 할인 불가라면 가장 할인율이 높은 결과로 리턴, 어떻게 줄이지
+            var max = 0;
+
+            for(var item of Discount_Results){
+                if(item > max){
+                    max = item
+                }
+            }
+            
+            this.discounted = Math.floor((oldAmount - max) / 10) * 10
+        },
+
+        getFilteredCards(cardType){
+            return this.creditCards.filter(item => item.cardType == cardType)
+        },
+
+        paddingBothSide(originText, maxLength=21, fillString=""){
+            const padCount = (maxLength - originText.length) / 2
+            const padString = `${fillString.repeat(padCount)}`
+
+            return `${padString}${originText}${padString}`
+        }
     },
 
 }
 </script>
 
-<style>
+<style scoped>
+
+div{
+    padding: 1px;
+    margin:10px;
+    border-radius: 15px;
+}
+
+.th{
+    font-size: 17px;
+    font-weight: bold;
+    color: rgb(109, 71, 0);
+    border-bottom: 2px dashed;
+}
+
+.th-head01{
+    width: 150px;
+}
+.th-head02{
+    width: 100px;
+}
+.th-head03{
+    width: 40px;
+}
+
+
+.item-font{
+    color:rgb(66, 27, 0);
+    font-size: 12px;
+    font-weight: bold;
+}
+.btn{
+    margin:10px;
+    padding: 5px;
+    width: 160px;
+    height:100px;
+    background: rgb(29, 28, 28);
+    font-size: 12px;
+    font-weight: bold;
+    border-radius: 10px;
+    border-color: rgb(78, 105, 14);
+}
+.MN_ORDER_BOOK{
+    background: rgb(255, 203, 107);
+    width: 580px;
+    border:2px solid;
+    border-color: #dcff12;
+    border-radius: 10px;
+}
+
+.big-white{
+    font-size: 25px;
+    font-weight: bold;
+    color:rgb(255, 255, 255);
+}
+.result{
+    border: 3px;
+    border-color: azure;
+    width: 580px;
+    height: 80px;
+    background-color: rgb(59, 59, 59);
+    
+}
+.MENU_BOARD{
+   background: rgb(194, 194, 194); 
+}
+.container{
+    border: 2px solid;
+    width: 600px;
+    height:1040px;
+    background: rgb(37, 25, 25);
+}
 
 </style>
